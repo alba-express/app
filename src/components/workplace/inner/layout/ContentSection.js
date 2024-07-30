382ba1afbc5f6a3788673ac664b4619be9c1e463
sedica3340 <sedica3340@gmail.com>
2024-07-30 16:08:10 +0900

    틀변경

--- src/components/workplace/inner/layout/ContentSection.js
+++ src/components/workplace/inner/layout/ContentSection.js
@@ -2,11 +2,12 @@ import React from "react";
 import InnerNavi from "./InnerNavi";
 import { Outlet } from "react-router-dom";
 import InnerContent from "./InnerContent";
+import InnerHeader from "./InnerHeader";
 
 const ContentSection = () => {
     return (
-        <div style={{ background: "skyblue", width: "100%", height: "80%", display: "flex" }}>
-            <InnerNavi />
+        <div style={{ background: "skyblue", width: "78%", height: "80%" }}>
+            <InnerHeader />
             <InnerContent />
         </div>
     );
--- src/components/workplace/inner/layout/DetailBase.js
+++ src/components/workplace/inner/layout/DetailBase.js
@@ -2,10 +2,11 @@ import React from "react";
 import styles from "./DetailBase.module.scss";
 import InnerHeader from "./InnerHeader";
 import ContentSection from "./ContentSection";
+import InnerNavi from "./InnerNavi";
 const DetailBase = () => {
     return (
         <div className={styles.fullScreen}>
-            <InnerHeader />
+            <InnerNavi />
             <ContentSection />
         </div>
     );
