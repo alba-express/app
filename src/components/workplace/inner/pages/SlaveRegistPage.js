9d982e30891fc4b056926d9a506a72d61186bbec
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-31 16:54:59 +0900

    ✅ 직원 등록페이지 디자인 만드는 중 01

--- src/components/workplace/inner/pages/SlaveRegistPage.js
+++ src/components/workplace/inner/pages/SlaveRegistPage.js
@@ -1,9 +1,15 @@
 import React from "react";
+import styles from './SlaveRegistPage.module.scss'
 
 const SlaveRegistPage = () => {
     return (
         <>
-            <p>직원등록</p>
+            <div className={styles['SlaveRegistPage']}>
+                <div className={styles['SlaveRegistPageHeader-box']}>
+                    직원등록
+                </div>
+                
+            </div>
         </>
     );
 };
