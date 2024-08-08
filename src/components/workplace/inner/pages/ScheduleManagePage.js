4e22141e767bb5c36361ccedb8c9844023b24d95
SoobihnKim <ksbsj92@naver.com>
2024-08-08 10:05:40 +0900

    일정관리 기본 구조

--- src/components/workplace/inner/pages/ScheduleManagePage.js
+++ src/components/workplace/inner/pages/ScheduleManagePage.js
@@ -1,9 +1,73 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import styles from "./ScheduleManagePage.module.scss";
 
 const ScheduleManagePage = () => {
+
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [days, setDays] = useState([]);
+
+    useEffect(() => {
+        createCalendar(currentDate);
+    }, [currentDate]);
+
+    const createCalendar = (date) => {
+        const year = date.getFullYear();
+        const month = date.getMonth();
+
+        const firstDayOfMonth = new Date(year, month, 1).getDay();
+        const lastDayOfMonth = new Date(year, month + 1, 0).getDate();
+
+        const daysArray = [];
+        for (let i = 0; i < firstDayOfMonth; i++) {
+            daysArray.push(null);
+        }
+        for (let date = 1; date <= lastDayOfMonth; date++) {
+            daysArray.push(date);
+        }
+        setDays(daysArray);
+    };
+
+    const handlePrevMonth = (e) => {
+        setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1));
+    };
+
+    const handleNextMonth = (e) => {
+        setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1));
+    };
+
+    const monthName = currentDate.toLocaleString('default', {month: 'long'});
+    const year = currentDate.getFullYear();
+
     return (
         <>
-            <p>일정관리 페이지</p>
+            <div className={styles.scheduleTitle}>
+                <h1>일정관리</h1>
+            </div>
+            <div className={styles.schedule}>
+                <div className={styles.calendar}>
+                    <div className={styles.calendarHeader}>
+                        <button onClick={handlePrevMonth}>◀</button>
+                        <h2>{monthName} {year}</h2>
+                        <button onClick={handleNextMonth}>▶</button>
+                    </div>
+                    <div className={styles.calendarBody}>
+                        {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map(day => (
+                            <div key={day} className={styles.day}>{day}</div>
+                        ))}
+                        {days.map((day, index) => (
+                            <div key={index} className={styles.day}>
+                                {day}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <div className={styles.todaySchedule}>
+                    <h2>오늘의 근무자</h2>
+                    <p>총 5명</p>
+
+                </div>
+            </div>
         </>
     );
 };
--- src/components/workplace/outer/pages/WorkplaceListPage.js
+++ src/components/workplace/outer/pages/WorkplaceListPage.js
@@ -8,75 +8,77 @@ const WorkplaceListPage = () => {
 //   재한님 코드 추가
   const navigate = useNavigate();
 
-    const handleLogout = () => {
-        // JWT 토큰 삭제
-        removeUserToken();
-        // 로그인 페이지로 리다이렉트
-        navigate("/login");
+  const handleLogout = () => {
+    // JWT 토큰 삭제
+    removeUserToken();
+    // 로그인 페이지로 리다이렉트
+    navigate("/login");
+  }
 //       끝
-  
-  const [workplaces, setWorkplaces] = useState([]);
 
-  useEffect(() => {
-    // API에서 데이터를 가져오는 함수
-    const fetchWorkplaces = async () => {
-      try {
-        const response = await axios.get(`http://localhost:8877/workplace/list/1`);
-        setWorkplaces(response.data.workplaces);
-      } catch (error) {
-        console.error('Error fetching workplace data:', error);
+    const [workplaces, setWorkplaces] = useState([]);
+
+    useEffect(() => {
+      // API에서 데이터를 가져오는 함수
+      const fetchWorkplaces = async () => {
+        try {
+          const response = await axios.get(`http://localhost:8877/workplace/list/1`);
+          setWorkplaces(response.data.workplaces);
+        } catch (error) {
+          console.error('Error fetching workplace data:', error);
+        }
+      };
+
+      fetchWorkplaces();
+    }, []);
+
+    const deleteHandler = async (id, name) => {
+      const confirmed = window.confirm(`[${name}] 업장을 삭제하시겠습니까 ❓`);
+      if (confirmed) {
+        try {
+          await axios.delete(`http://localhost:8877/workplace/delete/${id}`);
+          setWorkplaces((prevWorkplaces) => prevWorkplaces.filter(workplace => workplace.id !== id));
+        } catch (error) {
+          console.error('Error deleting workplace:', error);
+        }
       }
     };
 
-    fetchWorkplaces();
-  }, []);
+    return (
+        <>
+          <Link to="regist">
+            <button>사업장 등록</button>
+            <br/>
+          </Link>
 
-  const deleteHandler = async (id, name) => {
-    const confirmed = window.confirm(`[${name}] 업장을 삭제하시겠습니까 ❓`);
-    if (confirmed) {
-    try {
-      await axios.delete(`http://localhost:8877/workplace/delete/${id}`);
-      setWorkplaces((prevWorkplaces) => prevWorkplaces.filter(workplace => workplace.id !== id));
-    } catch (error) {
-      console.error('Error deleting workplace:', error);
-    }
-    }
+          <div>
+            <h1>사업장 목록</h1>
+            {workplaces.length === 0 ? (
+                <p>등록된 사업장이 존재하지 않습니다.</p>
+            ) : (
+                <ul>
+                  {workplaces.map(workplace => (
+                      <li key={workplace.id} style={{marginBottom: '20px'}}>
+                        <h2>{workplace.workplaceName}</h2>
+                        <p>주소: {`${workplace.workplaceAddressStreet} ${workplace.workplaceAddressDetail}`}</p>
+                        <p>사업장 규모: {workplace.workplaceSize ? '5인 이상' : '5인 미만'}</p>
+                        <p>등록일: {new Date(workplace.workplaceCreatedAt).toLocaleDateString()}</p>
+                        <Link to={`modify/${workplace.id}`}>
+                          <button>사업장 수정</button>
+                        </Link>
+
+                        <button onClick={() => deleteHandler(workplace.id, workplace.workplaceName)}>사업장 삭제</button>
+
+                      </li>
+                  ))}
+                </ul>
+            )}
+          </div>
+          <button onClick={handleLogout}>
+            로그아웃
+          </button>
+        </>
+    );
   };
-
-  return (
-    <>
-    <Link to="regist">
-      <button>사업장 등록</button> <br />
-    </Link>
-    
-    <div>
-      <h1>사업장 목록</h1>
-      {workplaces.length === 0 ? (
-        <p>등록된 사업장이 존재하지 않습니다.</p>
-      ) : (
-        <ul>
-          {workplaces.map(workplace => (
-            <li key={workplace.id} style={{ marginBottom: '20px' }}>
-              <h2>{workplace.workplaceName}</h2>
-              <p>주소: {`${workplace.workplaceAddressStreet} ${workplace.workplaceAddressDetail}`}</p>
-              <p>사업장 규모: {workplace.workplaceSize ? '5인 이상' : '5인 미만'}</p>
-              <p>등록일: {new Date(workplace.workplaceCreatedAt).toLocaleDateString()}</p>
-              <Link to={`modify/${workplace.id}`}>
-                <button>사업장 수정</button>
-              </Link>
-              
-                <button onClick={() => deleteHandler(workplace.id, workplace.workplaceName)}>사업장 삭제</button>
-              
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-    <button onClick={handleLogout}>
-                로그아웃
-    </button>
-    </>
-  );
-};
 
 export default WorkplaceListPage;
