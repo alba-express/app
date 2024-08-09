d76c7cd6ff454c6eedafd06ad7aaf85d4f6e9b99
junglimit <jaehan_jjh@naver.com>
2024-08-09 15:22:23 +0900

    로그인 되어있으면 설명충페이지 못가게 막기

--- src/components/app-pages/Home.js
+++ src/components/app-pages/Home.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import MainHeader from "../app-layout/MainHeader";
-
+import { getUserId } from "../../utils/auth"; // auth 유틸리티 함수 가져오기
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const userId = getUserId();
+        if (userId) {
+            // 로그인된 상태라면 /workplace로 리다이렉트
+            navigate('/workplace');
+        }
+    }, [navigate]);
+
     return (
         <>
-            <MainHeader isHome={true}/> 
-            <p >설명충 페이지</p>
-
+            <MainHeader isHome={true}/>
+            <p>설명충 페이지</p>
         </>
     );
 };
