a75fbfea40f196140cfbb89f19d349a9da4141bd
goodzeee <qkdtmdqo14@gmail.com>
2024-08-09 15:02:36 +0900

    # 사장 아이디 받아오기 & 사업장 아이디 보내기(reducer)

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import wageReducer from "./wage-slice";
 import noticeReducer from "./notice-slice";
+import workplaceReducer from "./workplace-slice";
 
 const store = configureStore({
     reducer: {
         wage: wageReducer,
         notice: noticeReducer,
+        workplace: workplaceReducer,
         
     }
 });
