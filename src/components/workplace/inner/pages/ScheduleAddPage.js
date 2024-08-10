b7306db8c1e3a82d044a76607aa0f0a91e28f93e
soo <ksbsj92@naver.com>
2024-08-10 16:14:10 +0900

    일정 추가

--- src/components/workplace/inner/pages/ScheduleAddPage.js
+++ src/components/workplace/inner/pages/ScheduleAddPage.js
@@ -17,7 +17,49 @@ const ScheduleAddPage = () => {
                                       setSelectedDate={setSelectedDate}/>
 
                 <div className={styles.modifySchedule}>
-                    <h2>일정 수정</h2>
+                    <h2>일정 추가</h2>
+
+                    <div className={styles.formGroup}>
+                        <label htmlFor="employeeSelect">직원 선택:</label>
+                        <select id="employeeSelect"
+                                // value={selectedEmployee}
+                                // onChange={handleEmployeeChange}
+                        >
+                            <option value="">직원을 선택하세요</option>
+                            {/*{employees.map((employee, index) => (*/}
+                            {/*    <option key={index} value={employee}>{employee}</option>*/}
+                            {/*))}*/}
+                        </select>
+                    </div>
+
+                    <div className={styles.formGroup}>
+                        <label htmlFor="daySelect">요일 선택:</label>
+                        <select id="daySelect"
+                                // value={selectedDay}
+                                // onChange={handleDayChange}
+                        >
+                            <option value="">요일을 선택하세요</option>
+                            {/*{daysOfWeek.map((day, index) => (*/}
+                            {/*    <option key={index} value={day}>{day}</option>*/}
+                            {/*))}*/}
+                        </select>
+                    </div>
+
+                    <div className={styles.formGroup}>
+                        <label htmlFor="startTime">시작 시간:</label>
+                        <input type="time" id="startTime"
+                               // value={startTime}
+                               // onChange={handleStartTimeChange}
+                        />
+                    </div>
+
+                    <div className={styles.formGroup}>
+                        <label htmlFor="endTime">종료 시간:</label>
+                        <input type="time" id="endTime"
+                               // value={endTime}
+                               // onChange={handleEndTimeChange}
+                        />
+                    </div>
 
                 </div>
             </div>
