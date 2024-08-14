1737a34d74f5e5f01e87c290b8b9a3b79de66eec
yunjeongg <bae.yunjeongg@gmail.com>
2024-08-14 12:49:56 +0900

    ✅ 직원 한 명 조회하기 02 직원상세정보페이지 새로고침했을 때 직원정보 사라지는 문제 해결

--- src/components/workplace/inner/pages/SlaveInfoPage.js
+++ src/components/workplace/inner/pages/SlaveInfoPage.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './SlaveInfoPage.module.scss';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import SlaveInfoPageCommuteList from './slave/SlaveInfoPageCommuteList';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { slaveActions } from '../../../../store/slave-slice';
 
 const SlaveInfoPage = () => {
+
+    // SlaveManagePageSlaveStatusList 에서 useNavigate 를 통해 이동할 때 같이 전달한 state 받기
+    // useLocation Hook 변수에 저장하기
+    const location = useLocation();
+
+    // useLocation 를 통해 state 꺼내기
+    const slaveId = location.state.slaveId;
+
+    // console.log('location으로 전달받은 state에 들어있는 내가 클릭한 직원의 id', slaveId);
+
+    //-------------------------------------------------
+
+    // useDispatch Hook 변수에 저장하기
+    const dispatch = useDispatch();
+
+    // redux store 에서 특정 직원 한 명의 정보를 표시하는 상태값 불러오기 (초기값: 특정 직원의 한 명의 정보를 넣을 빈 배열)
+    const showOneSlaveInfo = useSelector((state) => state.slave.showOneSlaveInfo);
+
+    // 에러 상태값으로 관리
+    const [error, setError] = useState(null);
+
+    //-------------------------------------------------
+
+    // 전달받은 직원의 id 를 서버로 전달해 해당 직원의 정보를 받아 redux store 에 저장하기
+    useEffect(() => {
+
+        const fetchSlaveInfo = async () => {
+            // 이 직원의 아이디를 서버로 전달하여 일치하는 id를 가진 직원을 찾기
+            try {
+                const response = await axios.get(`http://localhost:8877/detail/slave-info/${slaveId}`);
+
+                // redux store 에 직원정보 저장하기
+                dispatch(slaveActions.setShowOneSlaveInfo(response.data));
+
+            } catch (error) {
+                  setError(error.message);
+                  console.error('Error:', error);
+            }
+        }; 
+
+        if (slaveId) {
+            fetchSlaveInfo();
+          } else {
+            setError('직원 ID가 전달되지 않았습니다.');
+        }
+
+        // console.log(showOneSlaveInfo);
+
+    }, [slaveId, dispatch]);
+
+    if (error) {
+      return <div>Error: {error}</div>;
+    }
+
+    //-------------------------------------------------
+
   return (
     <>
         <div className={styles['content-box']}>
@@ -13,7 +72,6 @@ const SlaveInfoPage = () => {
                     <button className={styles['headerButton']} > 직원수정 </button>
                 </Link>
             </div>
-
             <div className={styles['slaveInfoPage-MiddleBox']}>
                 <div>
                     <div className={styles['slaveInfoPage-SlaveInfoBox']}>
@@ -27,21 +85,19 @@ const SlaveInfoPage = () => {
                                 <div> 입사일 : </div>
                             </div>
                             <div className={styles['slaveInfoPage-SlaveInfoContentContent']}>
-                                <div>뽀로로</div>
-                                <div>노예</div>
-                                <div>010-9542-4752</div>
-                                <div>1810-05-24</div>
-                                <div>2024-08-13</div>
+                                <div>{showOneSlaveInfo.slaveName}</div>
+                                <div>{showOneSlaveInfo.slavePosition}</div>
+                                <div>{showOneSlaveInfo.slavePhoneNumber}</div>
+                                <div>{showOneSlaveInfo.slaveBirthday}</div>
+                                <div>{showOneSlaveInfo.slaveCreatedAt}</div>
                             </div>
                         </div>
                     </div>
-
                     <div className={styles['slaveInfoPage-SlaveWageBox']}>
                         <div className={styles['slaveInfoPage-SlaveWageTitle']}> 급여타입 / 금액 </div>
                         <div className={styles['slaveInfoPage-SlaveInfoContentBox']} > 시급 10000원 </div>
                     </div>
                 </div>
-                    
                 <div>
                     <div className={styles['slaveInfoPage-SlaveScheduleBox']}>
                         <div className={styles['slaveInfoPage-SlaveScheduleTitle']}> 고정근무시간 </div>
@@ -57,16 +113,13 @@ const SlaveInfoPage = () => {
                         </div>
                     </div>
                 </div>
-
                 <div>
                     <div className={styles['slaveInfoPage-SlaveNothingBox']}>
                         <div className={styles['slaveInfoPage-SlaveNothingTitle']}> 임시제목 </div>
                         <div className={styles['slaveInfoPage-SlaveNothingContentBox']}> 임시내용 </div>
                     </div>
                 </div>
-
             </div>
-
             <div className={styles['slaveInfoPage-BottomBox']} >
                 <div className={styles['slaveInfoPage-BottomTitle']}> 근태정보 </div>
                 <SlaveInfoPageCommuteList />
