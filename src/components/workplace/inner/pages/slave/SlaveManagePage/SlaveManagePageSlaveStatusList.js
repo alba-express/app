676c15986ec3ad94929791227ba1330232c0d8ce
yunjeongg <bae.yunjeongg@gmail.com>
2024-08-23 15:21:25 +0900

    ✅ 디자인 05. 직원상세페이지 수정

--- src/components/workplace/inner/pages/slave/SlaveManagePage/SlaveManagePageSlaveStatusList.js
+++ src/components/workplace/inner/pages/slave/SlaveManagePage/SlaveManagePageSlaveStatusList.js
@@ -173,7 +173,8 @@ const SlaveManagePageSlaveStatusList = () => {
               style={{
                 height: oneSlave.slaveScheduleList.some(schedule => schedule.scheduleType === false)
                   ? `${oneSlave.slaveScheduleList.length * 1.8}rem`
-                  : 'auto' // 조건을 만족하지 않을 때의 기본 높이 설정
+                  : '2rem',// 조건을 만족하지 않을 때의 기본 높이 설정
+                  minHeight: '2.7rem'
               }}
               className={`${styles['link-text']} ${styles['slaveManagementList-OneSlave']}`}
             >
